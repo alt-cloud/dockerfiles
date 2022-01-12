767cb14e9239a6bd31f2b088dd3cfde476eddb30
Alexey Kostarev <kaf@altlinux.org>
2022-01-12 10:39:40 +0500

    Delete quay as subdir

--- quayservices/quay/active_migration.py
+++
@@ -1,4 +0,0 @@
-from enum import Enum, unique
-from data.migrationutil import DefinedDataMigration, MigrationPhase
-
-ActiveDataMigration = None
--- quayservices/quay/workers/gc/test/test_gcworker.py
+++
@@ -1,8 +0,0 @@
-from workers.gc.gcworker import GarbageCollectionWorker
-
-from test.fixtures import *
-
-
-def test_gc(initialized_db):
-    worker = GarbageCollectionWorker()
-    worker._garbage_collection_repos(skip_lock_for_testing=True)
